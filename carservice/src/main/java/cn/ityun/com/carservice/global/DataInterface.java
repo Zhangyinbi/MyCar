23b16ef3cf984a0cc03412e7aa753e2dc23733a9
yinbi <1160118373@qq.com>
2016-07-29 19:53:21 +0800

    2016-7-29   20:00

--- carservice/src/main/java/cn/ityun/com/carservice/global/DataInterface.java
+++ carservice/src/main/java/cn/ityun/com/carservice/global/DataInterface.java
@@ -4,7 +4,7 @@ package cn.ityun.com.carservice.global;
  * Created by Administrator on 2016/6/28 0028.
  */
 public class DataInterface {
-    public static final String PROVINCE_URL = "http://api.jisuapi.com/illegal/carorg?appkey=4e3ada1ff0498822&onlysupport=1";
+    public static final String PROVINCE_URL = "http://api.jisuapi.com/illegal/carorg?appkey=4e3ada1ff0498822&onlysupport=one";
     public static final String CAR_SIZE_URL = "http://api.jisuapi.com/illegal/lstype?appkey=4e3ada1ff0498822";
     public static final String SERVER_UPLOAD="http://172.16.14.100:8080/CarMarket/";
 
